670fd51485b7e9c3dc7633cff32df8d0d5fef2a4
Corey Bruyere <coreybruyere@ymail.com>
2018-11-08 09:15:55 -0800

    Update RSVP form to handle navigation after submit

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Flex, Box, Text, Heading } from "rebass";
-import NetlifyForm from "react-netlify-form";
+import { navigateTo } from "gatsby-link";
 import { FormattedMessage } from "react-intl";
 import {
   Layout,
@@ -15,112 +15,163 @@ import {
 import SEO from "../components/common/SEO";
 import Header from "../components/theme/Header";
 
-const RSVPPage = () => (
-  <Layout>
-    <React.Fragment>
-      <SEO title="rsvp" />
-      <Header />
-      <Welcome as={Container}>
-        <Heading as="h1">
-          <FormattedMessage id="rsvp" />
-        </Heading>
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-        <form
-          name="rsvp"
-          method="post"
-          action="/rsvp-success/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="bot-field" />
+class RSVPPage extends React.Component {
+  state = {};
 
-          <Flex mx={-2} flexWrap="wrap">
-            <Box px={2} width={1 / 2}>
-              <Field>
-                <Label>
-                  <Text fontSize={1} fontWeight="bold" color="#616161">
-                    First Name
-                  </Text>
-                  <input type="text" name="first-name" required />
-                </Label>
-              </Field>
-            </Box>
-            <Box px={2} width={1 / 2}>
-              <Field>
-                <Label>
-                  <Text fontSize={1} fontWeight="bold" color="#616161">
-                    Last Name
-                  </Text>
-                  <input type="text" name="last-name" required />
-                </Label>
-              </Field>
-            </Box>
-            <Box px={2} width={1 / 2}>
-              <fieldset>
-                <legend>Will you be attending?</legend>
-                <Field>
-                  <Label>
-                    <input
-                      type="radio"
-                      id="yes"
-                      name="attend"
-                      value="yes"
-                      checked
-                      required
-                    />
-                    Yes
-                  </Label>
-                </Field>
-                <Field>
-                  <Label>
-                    <input
-                      type="radio"
-                      id="no"
-                      name="attend"
-                      value="no"
-                      checked
-                    />
-                    No
-                  </Label>
-                </Field>
-              </fieldset>
-            </Box>
-            <Box px={2} width={1}>
-              <Field>
-                <Label>
-                  <Text fontSize={1} fontWeight="bold" color="#616161">
-                    Full Names of Additional Guests
-                  </Text>
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
+  render() {
+    return (
+      <Layout>
+        <React.Fragment>
+          <SEO title="rsvp" />
+          <Header />
+          <Welcome as={Container}>
+            <Heading as="h1">
+              <FormattedMessage id="rsvp" />
+            </Heading>
+
+            <form
+              name="rsvp"
+              method="post"
+              action="/rsvp-success/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={this.handleSubmit}
+            >
+              <input type="hidden" name="form-name" value="contact" />
+
+              <input
+                type="hidden"
+                name="bot-field"
+                onChange={this.handleChange}
+              />
+
+              <Flex mx={-2} flexWrap="wrap">
+                <Box px={2} width={1 / 2}>
                   <Field>
-                    <textarea
-                      name="Guests"
-                      placeholder="Comma-separated list of names"
-                    />
+                    <Label>
+                      <Text fontSize={1} fontWeight="bold" color="#616161">
+                        First Name
+                      </Text>
+                      <input
+                        type="text"
+                        name="first-name"
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Label>
                   </Field>
-                </Label>
-              </Field>
-            </Box>
-            <Box px={2} width={1}>
-              <Field>
-                <Label>
-                  <Text fontSize={1} fontWeight="bold" color="#616161">
-                    Song Requests
-                  </Text>
+                </Box>
+                <Box px={2} width={1 / 2}>
                   <Field>
-                    <textarea name="Songs" />
+                    <Label>
+                      <Text fontSize={1} fontWeight="bold" color="#616161">
+                        Last Name
+                      </Text>
+                      <input
+                        type="text"
+                        name="last-name"
+                        onChange={this.handleChange}
+                        required
+                      />
+                    </Label>
                   </Field>
-                </Label>
-              </Field>
-            </Box>
-            <Box px={2} width={1}>
-              <Button type="submit">Submit</Button>
-            </Box>
-          </Flex>
-        </form>
-      </Welcome>
-    </React.Fragment>
-  </Layout>
-);
+                </Box>
+                <Box px={2} width={1 / 2}>
+                  <fieldset>
+                    <legend>Will you be attending?</legend>
+                    <Field>
+                      <Label>
+                        <input
+                          type="radio"
+                          id="yes"
+                          name="attend"
+                          value="yes"
+                          checked
+                          onChange={this.handleChange}
+                          required
+                        />
+                        Yes
+                      </Label>
+                    </Field>
+                    <Field>
+                      <Label>
+                        <input
+                          type="radio"
+                          id="no"
+                          name="attend"
+                          value="no"
+                          checked
+                          onChange={this.handleChange}
+                        />
+                        No
+                      </Label>
+                    </Field>
+                  </fieldset>
+                </Box>
+                <Box px={2} width={1}>
+                  <Field>
+                    <Label>
+                      <Text fontSize={1} fontWeight="bold" color="#616161">
+                        Full Names of Additional Guests
+                      </Text>
+                      <Field>
+                        <textarea
+                          name="Guests"
+                          placeholder="Comma-separated list of names"
+                          onChange={this.handleChange}
+                        />
+                      </Field>
+                    </Label>
+                  </Field>
+                </Box>
+                <Box px={2} width={1}>
+                  <Field>
+                    <Label>
+                      <Text fontSize={1} fontWeight="bold" color="#616161">
+                        Song Requests
+                      </Text>
+                      <Field>
+                        <textarea name="Songs" onChange={this.handleChange} />
+                      </Field>
+                    </Label>
+                  </Field>
+                </Box>
+                <Box px={2} width={1}>
+                  <Button type="submit">Submit</Button>
+                </Box>
+              </Flex>
+            </form>
+          </Welcome>
+        </React.Fragment>
+      </Layout>
+    );
+  }
+}
 
 const Welcome = styled.div`
   padding: 2rem 0;
