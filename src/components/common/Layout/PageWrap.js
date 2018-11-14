506772a23f30d0f3b543830753830f89d572872d
Corey Bruyere <coreybruyere@ymail.com>
2018-11-13 21:03:00 -0800

    Make sidebar more narrow

--- src/components/common/Layout/PageWrap.js
+++ src/components/common/Layout/PageWrap.js
@@ -5,7 +5,8 @@ export const PageWrap = styled.div`
   grid-template-columns: 100%;
 
   @media only screen and (min-width: 48em) {
-    grid-template-columns: 71% 29%;
+    // grid-template-columns: 71% 29%;
+    grid-template-columns: 75% 25%;
   }
 }
 `;
