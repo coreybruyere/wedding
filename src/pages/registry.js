69156ee6f7262384f5143c67907c808ce7acac86
Corey Bruyere <coreybruyere@ymail.com>
2019-01-21 21:43:05 -0800

    Add basic Registry details

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text } from "rebass";
+import { Text, Card, Flex } from "rebass";
 import { FormattedMessage } from "react-intl";
 import { Layout, Container, Main, Section } from "../components/common";
 import SEO from "../components/common/SEO";
@@ -16,8 +16,46 @@ const RegistryPage = () => (
             <FormattedMessage id="registry" />
           </Text>
 
-          <Section>
-            [[Boxed links to registry - Make sure to translate]]
+          <Section id="registry">
+            <Text as="p">
+              <FormattedMessage id="registry_details" />
+            </Text>
+
+            <Flex flexWrap="wrap">
+              <Card
+                width={1}
+                mb={4}
+                bg="#fff"
+                color="#403b32"
+                borderRadius={8}
+                border="4px solid"
+                borderColor="rgba(253,0,34,0.25098)"
+                boxShadow="0 2px 16px rgba(0,0,0,0.25)"
+                as="a"
+                target="_blank"
+                href="https://www.zola.com/registry/coreyandtori2019"
+                css={{ textDecoration: "none" }}
+              >
+                Honeymoon Cash Fund
+              </Card>
+
+              <Card
+                width={1}
+                mb={4}
+                bg="#fff"
+                color="#403b32"
+                borderRadius={8}
+                border="4px solid"
+                borderColor="rgba(253,0,34,0.25098)"
+                boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+                as="a"
+                target="_blank"
+                href="https://www.target.com/gift-registry/gift/corey-tori-2019/"
+                css={{ textDecoration: "none" }}
+              >
+                Target Registry
+              </Card>
+            </Flex>
           </Section>
         </Container>
       </Main>
